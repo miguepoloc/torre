baa918cb4c9af9f663de0a35e36d6dad7ead4700
miguepoloc <miguepoloc@gmail.com>
2020-09-20 17:56:15 -0500

    Cambios para Heroku y postgreSQL

--- torre/settings.py
+++ torre/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'torre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'torre',
+        'USER': 'admin',
+        'PASSWORD': 'Contrasena1!',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
