4a302b534acd9a8c928c436efa1a1e7234ede8c9
miguepoloc <miguepoloc@gmail.com>
2020-11-12 11:09:24 -0500

    Ultimo dato en el administrador

--- apps/api/admin.py
+++ apps/api/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Jugador, Children
+from .models import Jugador, Children, UltimoDato
 
 admin.site.register(Jugador)
 admin.site.register(Children)
+admin.site.register(UltimoDato)
